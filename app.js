0001f54d9ed16c54f447dac258939e0d66a20fde
sadiquex <abubakasaddik1@gmail.com>
2022-06-26 09:11:59 +0000

    displayed jokes in dom

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const jokeElement = document.getElementById('joke');
 const button = document.getElementById('jokeBtn');
 
+button.addEventListener('click', generateJoke)
 generateJoke()
 
-function generateJoke(){
+// function generateJoke(){
+//     const config = {
+//         headers : {
+//             'Accept': 'application/json'
+//         }
+//     }
+    
+//     fetch('https://icanhazdadjoke.com', config)
+//     .then(response => response.json())
+//     .then(data => {
+//         jokeElement.innerHTML = data.joke
+//     })
+// }
+
+
+// async await method(cleaner)
+
+async function generateJoke(){
     const config = {
         headers : {
             'Accept': 'application/json'
         }
     }
-    fetch('http://icanhazdadjoke.com', config).then(response => response.json.then(data => console.log(data)))
+    
+    const response = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await response.json()
+
+    jokeElement.innerHTML = data.joke
+    
 }
